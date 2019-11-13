01adeb20b2bde457e0b3248a248805218240ef79
Mikhail (Krony) Kolodin <Mikhail.Kolodin@kronshtadt.ru>
2019-11-13 16:38:30 +0300

    image naming ok

--- apserv-1/engine/script.py
+++ apserv-1/engine/script.py
@@ -1,3 +1,22 @@
 #!/usr/bin/env python
 
-print ("Hello to all!")
+#print ("Hello to all!")
+
+import datetime
+
+import bottle
+#from bottle import get, put, route, run, debug, app
+
+app = bottle.app()
+
+dt = datetime.datetime.now()
+dtstr = str(dt)
+
+@bottle.route('/')
+def index ():
+    return f"hello at {dtstr}"
+
+if __name__ == '__main__':
+    bottle.debug (True)
+    bottle.run (app=app, host='localhost', port=8080)
+
