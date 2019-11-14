7f0d297ce281ab81edf7f455b1a284ab5d16eb4f
Mikhail (Krony) Kolodin <Mikhail.Kolodin@kronshtadt.ru>
2019-11-14 16:19:09 +0300

    ver 1.2 ok with gunicorn

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # damba
 These are the ways to the island
 
-2019-11-11 2019-11-12 v.0.4
+2019-11-11 2019-11-14 v.1.0
 Some tests for Damba project
--- apserv-1/engine/script.py
+++ apserv-1/engine/script.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 # main runner of damba engine
-# ver. 1.0. ok 2019-11-13
+# ver. 1.2. ok 2019-11-14
 
 import datetime
 
-import bottle
-#from bottle import get, put, route, run, debug, app, template, Bottle
+#import bottle
+from bottle import get, put, route, run, debug, app, template, Bottle
 
-app = bottle.Bottle()
+app = Bottle()
 
 dt = datetime.datetime.now()
 dtstr = str(dt)
-print ("damba. starting at %s\n" % (dtstr, ))
+print ("damba. starting at %s\n" % (dtstr,))
 
 @app.get('/')
 def index ():
@@ -20,8 +20,10 @@ def index ():
     return "hello at %s" % (dtstr,)
 
 if __name__ == '__main__':
+    app.run (server='gunicorn', host='0.0.0.0', port=8080)
+
 #    app.debug (True)
-    app.run (host='0.0.0.0', port=8080)
+#    app.run (host='0.0.0.0', port=8080)
 #    app.run (host='0.0.0.0', port=8080, server=cgi)
 #    app.run (host='localhost', port=8080)
 
