eb22ef0122852b2091401c8572f8c27e08adb9c4
Mikhail (myke) Kolodin <mykespb@gmail.com>
2019-11-18 17:55:24 +0300

    added new ulid marks, restructured docker files

--- apserv-1/engine/script.py
+++ apserv-1/engine/script.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # main runner of damba engine
-# ver. 1.4. ok 2019-11-18
+# ver. 1.6. ok 2019-11-18
 
 import datetime
+import ulid
 
 #import bottle
 from bottle import get, put, route, run, debug, app, template, Bottle
@@ -17,10 +18,12 @@ print ("damba engine. starting at %s\n" % (dtstr,))
 def index ():
     dt = datetime.datetime.now()
     dtstr = str(dt)
-    return "hello from engine at %s" % (dtstr,)
+    myulid = ulid.new().str
+
+    return "<tt>hello from engine at %s as %s [len%d]</tt>" % (dtstr, myulid, len(myulid))
 
 if __name__ == '__main__':
-    app.run (server='gunicorn', host='0.0.0.0', port=8080)
+    app.run (server='gunicorn', host='0.0.0.0', port=8080, debug=True)
 
 #    app.debug (True)
 #    app.run (host='0.0.0.0', port=8080)
