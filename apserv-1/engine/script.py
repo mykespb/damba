452b75510ef4c9b4d35c8116a0f35fd71bbcd993
Mikhail (myke) Kolodin <mykespb@gmail.com>
2019-11-19 12:46:26 +0300

    added own short repr of ulid, ok

--- apserv-1/engine/script.py
+++ apserv-1/engine/script.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # main runner of damba engine
-# ver. 1.6. ok 2019-11-18
+# ver. 1.7. run 2019-11-19
 
 import datetime
 import ulid
@@ -14,18 +14,38 @@ dt = datetime.datetime.now()
 dtstr = str(dt)
 print ("damba engine. starting at %s\n" % (dtstr,))
 
+# --------------- bazed ULID
+def bazed_ulid(n):
+	baza = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	bl = len(baza)
+	res = ''
+	if n == 0:
+		return '0'
+	while n:
+		r = n % bl
+		n //= bl
+		res = baza[r] + res
+	return res
+
+# --------------- web service
 @app.get('/')
 def index ():
     dt = datetime.datetime.now()
     dtstr = str(dt)
-    myulid = ulid.new().str
+    myulid = ulid.new()
+    strmyulid = myulid.str
+    intmyulid = myulid.int
+    bmyulid = bazed_ulid(intmyulid)
 
-    return "<tt>hello from engine at %s as %s [len%d]</tt>" % (dtstr, myulid, len(myulid))
+    return "<tt>hello from engine at %s<br />as long %s [len%d] and short %s [len%d]</tt>" % (dtstr, strmyulid, len(strmyulid), bmyulid, len(bmyulid))
 
+# ---------------- caller
 if __name__ == '__main__':
     app.run (server='gunicorn', host='0.0.0.0', port=8080, debug=True)
 
 #    app.debug (True)
 #    app.run (host='0.0.0.0', port=8080)
 #    app.run (host='localhost', port=8080)
+
+# the end.
 
