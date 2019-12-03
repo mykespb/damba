de76e6a1e9c8693459c493767b1010921acf08f5
Mikhail (myke) Kolodin <mykespb@gmail.com>
2019-12-03 15:40:10 +0300

    better tests script

--- apserv-3-nginx/tests/test-10-10.sh
+++ apserv-3-nginx/tests/test-10-10.sh
@@ -1,2 +1,2 @@
-echo wrk -t10 -c10 -d20s http://localhost:8080 ... asgi-10-10.log
-wrk -t10 -c10 -d20s http://localhost:8080 > asgi-10-10.log
+echo wrk -t10 -c10 -d20s http://localhost:8080 ... nginx-10-10.log
+wrk -t10 -c10 -d20s http://localhost:8080 > nginx-10-10.log
